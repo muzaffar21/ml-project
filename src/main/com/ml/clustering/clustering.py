79198af9b240c2ad218f30123c8fe0e53cb840b3
Muzaffar Shah <muzaffarshah@simplyphi.com>
2017-02-18 19:23:29 +0530

    made cluster number as input

--- src/main/com/ml/clustering/clustering.py
+++ src/main/com/ml/clustering/clustering.py
@@ -5,8 +5,8 @@ import json
 from src.main.com.ml.classifier.text_vectorizer import Vectorizer
 
 
-def run_k_means(vec_matrix):
-    k_means = KMeans(n_clusters=8, random_state=0).fit(vec_matrix)
+def run_k_means(vec_matrix,num_clusters):
+    k_means = KMeans(n_clusters=, random_state=0).fit(vec_matrix)
     clusters = k_means.labels_
     return clusters
 
@@ -33,9 +33,9 @@ def create_result_file(clusters, corpus, result_file):
     fp.close()
 
 
-def main(file_path, result_path):
+def main(file_path, result_path,num_clusters):
     vectorizer = Vectorizer()
-    corpus = load_data(file_path)
+    corpus = load_data(file_path,num_clusters)
     vec_matrix = vectorizer.generate_vector_matrix(corpus, "not", False)
     clusters = run_k_means(vec_matrix)
     create_result_file(clusters, corpus, result_path)
@@ -43,4 +43,4 @@ def main(file_path, result_path):
 
 if __name__ == '__main__':
     directory = "/disk2/home/muzaffar/PycharmProjects/ml-project/resources/"
-    main(directory + "clustering-data.lst", directory + "clusters.json")
+    main(directory + "clustering-data.lst", directory + "clusters.json",8)
